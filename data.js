39d8f5d67c32af56a7f391485799b9a27cfe18e4
Rayturner677 <rturner@basecampcodingacademy.org>
2018-10-15 08:48:34 -0500

    first commit

---
+++ data.js
@@ -0,0 +1,20 @@
+const INVENTORY = {
+    camaro: {
+        name: "camaro",
+        per_day: 180,
+        stock: 4,
+        price: 40000
+    },
+    honda: {
+        name: "4-wheeler",
+        per_day: 29,
+        stock: 15,
+        price: 5000
+    },
+    jetSki: {
+        name: "jet-ski",
+        per_day: 29,
+        stock: 8,
+        price: 4000
+    }
+};
